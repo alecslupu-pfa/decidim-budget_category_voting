46f836286e663c4acb5f5ea0502092731db74935
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-02-07 02:07:37 +0200

    Add template

--- lib/decidim/budget_category_voting.rb
+++ lib/decidim/budget_category_voting.rb
@@ -3,10 +3,14 @@
 require "decidim/budget_category_voting/admin"
 require "decidim/budget_category_voting/engine"
 require "decidim/budget_category_voting/admin_engine"
+require "decidim/budget_category_voting/workflows"
 
 module Decidim
   # This namespace holds the logic of the `BudgetCategoryVoting` component. This component
   # allows users to create budget_category_voting in a participatory space.
   module BudgetCategoryVoting
+    module Overrides
+      autoload :SettingsHelper, "decidim/budget_category_voting/overrides/settings_helper"
+    end
   end
 end
--- lib/decidim/budget_category_voting/engine.rb
+++ lib/decidim/budget_category_voting/engine.rb
@@ -15,8 +15,23 @@ module Decidim
         # root to: "budget_category_voting#index"
       end
 
-      initializer "BudgetCategoryVoting.webpacker.assets_path" do
+      initializer "decidim_budget_category_voting.webpacker.assets_path" do
         Decidim.register_assets_path File.expand_path("app/packs", root)
+      end
+
+      initializer "decidim_budget_category_voting.add_workflow", after: "load_config_initializers" do |app|
+        Decidim::Budgets.workflows[:category] = Decidim::BudgetCategoryVoting::Workflows::CategoryVoting
+      end
+
+      initializer "decidim_budget_category_voting.patch_engine" do
+        config.to_prepare do
+          Decidim::Admin::SettingsHelper.prepend(Decidim::BudgetCategoryVoting::Overrides::SettingsHelper)
+        end
+
+        Decidim.find_component_manifest(:budgets).settings(:global) do |settings|
+          settings.attribute :vote_rule_category_voting_enabled, type: :boolean, default: false
+          settings.attribute :vote_category_voting, type: :array
+        end
       end
     end
   end
