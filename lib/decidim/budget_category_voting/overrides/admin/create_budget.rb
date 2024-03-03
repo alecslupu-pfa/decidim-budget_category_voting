3acb825d26c5b4e9bef23067aab42bf96f7454c8
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-03-03 09:46:41 +0200

    Add budget show

--- lib/decidim/budget_category_voting/overrides/admin/create_budget.rb
+++ lib/decidim/budget_category_voting/overrides/admin/create_budget.rb
@@ -18,7 +18,7 @@ module Decidim
             }
 
             @budget = Decidim.traceability.create!(
-              Budget,
+              Decidim::Budgets::Budget,
               form.current_user,
               attributes,
               visibility: "all"
