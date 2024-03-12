cf93d5864c9767a467adeb7b779471120de3b34d
Alexandru Emil Lupu <contact@alecslupu.ro>
2024-03-12 20:45:33 +0200

    Add percentage budget rule (#14)

--- lib/decidim/budget_category_voting/rules/percentage_rule.rb
+++ lib/decidim/budget_category_voting/rules/percentage_rule.rb
@@ -4,23 +4,31 @@ module Decidim
   module BudgetCategoryVoting
     module Rules
       class PercentageRule < GenericRule
+        include ActionView::Helpers::NumberHelper
+
         def caption = I18n.t("remaining_votes", scope: "decidim.budget_category_voting.rule")
 
-        #
-        # def minimum_projects = model.fetch("vote_selected_projects_minimum", 0).to_i
-        #
-        # def maximum_projects = model.fetch("vote_selected_projects_maximum", 0).to_i
-        #
-        # alias :total_allocation :maximum_projects
-        #
-        # def remaining_votes
-        #   count = maximum_projects - current_allocation
-        #   count >= 0 ? count : 0
-        # end
-        #
-        # def current_allocation
-        #   @current_allocation ||= current_order.projects_count_for_rule(model)
-        # end
+        def remaining_votes
+          count = computed_percentage - current_allocation
+          count >= 0 ? number_to_percentage(count) : 0
+        end
+
+        def current_allocation
+          @current_allocation ||= begin
+            currently_selected = current_order.projects_sum_for_rule(model)
+            (currently_selected.to_f / current_order.budget.total_budget) * 100
+          end
+        end
+
+        def total_allocation
+          computed_percentage * current_order.budget.total_budget / 100
+        end
+
+        private
+
+        def computed_percentage
+          model.fetch("vote_threshold_percent", 0).to_f
+        end
       end
     end
   end
